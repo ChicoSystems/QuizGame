74231a7db0ee6d3a0e06ba0e28d473fd1b7e75b4
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-12-14 18:42:30 -0800

    local login now works
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- routes/routes.js
+++ routes/routes.js
@@ -13,6 +13,13 @@ module.exports = function(app, passport){
     );
   });
 
+  //process the login form
+  app.post('/login', passport.authenticate('local-login',{
+    successRedirect : '/profile',
+    failureRedirect : '/login',
+    failureFlash     : true
+  }));
+
   //Signup Page
   app.get('/signup', function(req, res){
     res.render(
