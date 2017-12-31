2dc56bafb55a65bf579a02655acc8ebe6a88a473
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-12-31 11:24:26 -0800

    Added penalization for refreshing
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- public/javascripts/randomquestion.js
+++ public/javascripts/randomquestion.js
@@ -6,8 +6,18 @@ $(function(){
  
   //if answers.length < 12, refresh page, as an error will happen otherwise
   if(answersLength < 12){
+    //we don't want to penalize user when this happens
+    sessionStorage.removeItem("not_answered");
      loadNewQuestion();
   }
+
+  //check if user has answered previous question or not
+  if(sessionStorage.getItem("not_answered")){
+   // alert("previous question not answered");
+    //the player loaded a question, but did not attempt to answer it, penalize
+    penalize();
+  }
+  sessionStorage.setItem("not_answered", true);
 });
 
 
@@ -40,6 +50,8 @@ function reportProblemClicked(){
 
 //The user clicked an answer button.
 function answerClicked(indexClicked){
+  //the user has attempted to answer the question
+  sessionStorage.removeItem("not_answered");
   //alert("clicked answer: " + indexClicked);
   if(indexClicked == answerIndex){
     $('#'+indexClicked).removeClass("btn-outline-primary");
@@ -52,20 +64,7 @@ function answerClicked(indexClicked){
     for(var i = 0; i < 12; i++){
       $('#'+i).attr("disabled", "disabled");
     }
- 
-    //update score display
-    var scoreString = $("#score").text();
-    var score = parseFloat(scoreString)+5;
-    $("#score").text(score);
-   
-    //only make an ajax call to server if logged in
-    if(loggedIn){ 
-      $.get("/rightanswer", function(data, status){
-         setTimeout(loadNewQuestion, 500);
-     });
-    }else{
-      setTimeout(loadNewQuestion, 500);
-    }
+    reward(); 
     
     
  
@@ -74,17 +73,50 @@ function answerClicked(indexClicked){
     $('#'+indexClicked).addClass("btn-danger");
     //$('#'+indexClicked).attr("disabled", "disabled");
     $('#'+indexClicked).addClass("disabled");
-      
+    penalize();  
+  }
+
+}
+
+//rewards the players score
+function reward(){
+    //update score display
+    var scoreString = $("#score").text();
+    var score = parseFloat(scoreString)+5;
+    $("#score").text(score);
+    blinkScore("blinkGreen");    
+ 
+    //only make an ajax call to server if logged in
+    if(loggedIn){ 
+      $.get("/rightanswer", function(data, status){
+         setTimeout(loadNewQuestion, 500);
+     });
+    }else{
+      setTimeout(loadNewQuestion, 500);
+    }
+
+}
+
+function blinkScore(blinkColor){
+  var $score = $("#score");
+  $score.toggleClass(blinkColor);
+  setTimeout(function(){
+    $score.toggleClass(blinkColor);
+  }, 400);
+}
+
+//penalizes the players score
+function penalize(){
     //update score display
     var scoreString = $("#score").text();
     var score = parseFloat(scoreString)-1;
     $("#score").text(score);
+    blinkScore("blinkRed");
 
     //only make call to server if logged in
     if(loggedIn){
       $.get("/wronganswer", function(data, status){
       });
     }
-  }
 
 }
