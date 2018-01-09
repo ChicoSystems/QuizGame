ed083110c707e2717eb4906cc2de57f4d5b40307
Isaac Assegai <Isaac.A.Travers@gmail.com>
2018-01-08 17:37:58 -0800

    changed readme
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- linecount.sh
+++ linecount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i '/-LINECOUNT-/q' README.rd
-cloc --read-lang-def=config/cloc_def.txt --exclude-dir=node_modules . >> README.rd
+sed -i '/-LINECOUNT-/q' README.md
+cloc --read-lang-def=config/cloc_def.txt --exclude-dir=node_modules . >> README.md
