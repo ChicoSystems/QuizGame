b1ea97c3175c1491152043fcc512d72116be5b8a
Isaac Assegai <Isaac.A.Travers@gmail.com>
2018-01-08 17:59:35 -0800

    Edited linecount.sh
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- linecount.sh
+++ linecount.sh
@@ -2,4 +2,5 @@
 
 sed -i '/-LINECOUNT-/q' README.md
 cloc --read-lang-def=config/cloc_def.txt --exclude-dir=node_modules . >> README.md
-sed -i '/-LINECOUNT-/,+30 s/^/> /' README.md
+sed -i '/-LINECOUNT-/ s/^/```/' README.md
+sed -i '$ a ```' README.md
